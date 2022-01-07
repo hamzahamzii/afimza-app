6a949174059ea01c4ed9321b56dccec880235408
hmzaehmad <hmza.ehmad@gmail.com>
2022-01-07 23:52:12 +0500

    refactoring

--- server.js
+++ server.js
@@ -55,11 +55,10 @@ app.delete("/thoughts/:id", (req, res) => {
 // PRODUCTION CHECKS
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 // STARTING SERVER
 app.listen(PORT, "localhost", () => {
