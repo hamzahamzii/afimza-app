ff9987f230ae4e9c9435f8611ace7bcc3f975d29
hmzaehmad <hmza.ehmad@gmail.com>
2022-01-08 02:59:13 +0500

    routing issue fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import ThoughtsPage from "./pages/thoughts";
 import GalleryPage from "./pages/gallery";
