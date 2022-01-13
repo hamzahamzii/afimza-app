b98cb16637f1289a887b4f71d30224cf5612d5d9
hmzaehmad <hmza.ehmad@gmail.com>
2022-01-13 21:58:19 +0500

    home page changed on be

--- globals.js
+++ globals.js
@@ -1,6 +1,9 @@
 // GLOBAL VARS
 const PORT = process.env.PORT || 8000;
-const HOME = process.env.HOME_PAGE || "http://localhost:3000";
+const HOME =
+  process.env.NODE_ENV === "production"
+    ? "https://www.afimza.com"
+    : "http://localhost:3000";
 const dbURI =
   process.env.MONGODB_URI ||
   "mongodb+srv://hamzahamzii:Blah-12345@afimza.loxun.mongodb.net/afimza?retryWrites=true&w=majority";
