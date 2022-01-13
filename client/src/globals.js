ab8c1bc3129811fdd576a7529b5c119a45d8f743
hmzaehmad <hmza.ehmad@gmail.com>
2022-01-13 21:51:14 +0500

    end method changed

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,3 +1,6 @@
-const API_URL = process.env.API_URL || "http://localhost:8000";
+const API_URL =
+  process.env.NODE_ENV == "production"
+    ? "https://www.afimza.com"
+    : "http://localhost:8000";
 
 export { API_URL };
